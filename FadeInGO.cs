eacece41aaeea25a9ce09156c1dd3bc3576f963b
mikadoro13 <31672640+mikadoro13@users.noreply.github.com>
2023-02-23 01:22:14 +0200

    Add files via upload

---
+++ FadeInGO.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public static class ClassExtensionFadeIn
+{
+    public static List<GameObject> GetAllChilds(this GameObject Go)
+    {
+        List<GameObject> list = new List<GameObject>();
+        for (int i = 0; i < Go.transform.childCount; i++)
+        {
+            list.Add(Go.transform.GetChild(i).gameObject);
+        }
+        return list;
+    }
+}
+
+public class FadeInGO : MonoBehaviour
+{
+
+
+
+    /* 
+    enable gameobject to start fade in
+    */
+
+
+    public float fadeInSpeed = 0.001f;
+
+    public bool fadeInFinished;
+
+    Color albedoColor;
+
+    private float alphastate;
+
+
+    private void OnEnable()
+    {
+        fadeInFinished = false;
+        StartCoroutine(FadeInCoroutine());
+
+    }
+
+    private void OnDisable()
+    {
+        fadeInFinished = false;
+    }
+
+    public IEnumerator FadeInCoroutine()
+    {
+        Debug.Log("mpika stin fade");
+
+        
+       
+
+        //make every child's alpha channel to 0
+        foreach (var child in gameObject.GetAllChilds())
+        {
+            albedoColor = child.GetComponent<MeshRenderer>().material.color;
+            albedoColor.a = 0;
+            child.GetComponent<MeshRenderer>().material.color = albedoColor;
+
+        }
+
+        /*
+        albedoColor = GetComponent<MeshRenderer>().material.color;
+        albedoColor.a = 0;
+        GetComponent<MeshRenderer>().material.color = albedoColor;
+        */
+
+        albedoColor.a = 255;
+
+        while (alphastate < 1)
+        {
+            foreach (var child in gameObject.GetAllChilds())
+            {
+                child.GetComponent<MeshRenderer>().material.color = Color.Lerp(child.GetComponent<MeshRenderer>().material.color, albedoColor, fadeInSpeed * Time.deltaTime);
+                alphastate = child.GetComponent<MeshRenderer>().material.color.a;
+                Debug.Log(alphastate);
+            }
+            yield return new WaitForEndOfFrame();
+
+            /*
+            GetComponent<MeshRenderer>().material.color = Color.Lerp(GetComponent<MeshRenderer>().material.color, albedoColor, fadeInSpeed * Time.deltaTime);
+            //Debug.Log("mpika stin while");
+            */
+
+        }
+        
+        
+        fadeInFinished = true;
+    }
+
+
+    
+
+}
---
+++ FadeOutGO.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class ClassExtensionFadeOut
+{
+    static List<GameObject> GetAllChilds(this GameObject Go)
+    {
+        List<GameObject> list = new List<GameObject>();
+        for (int i = 0; i < Go.transform.childCount; i++)
+        {
+            list.Add(Go.transform.GetChild(i).gameObject);
+        }
+        return list;
+    }
+}
+
+public class FadeOutGO : MonoBehaviour
+{
+    public bool StartFadeOut;
+    public float fadeOutSpeed = 0.8f;
+    public bool DisableOnFadeOut = true;
+    public bool DestroyOnFadeOut;
+    public bool FadeOutPending;
+
+    
+    Color albedoColor;
+    float alphaState = 1;
+
+    private void Update()
+    {
+        if (StartFadeOut == true && FadeOutPending == false)
+        {
+            FadeOutPending = true;
+            StartCoroutine(FadeOutCoroutine());
+        }
+    }
+
+
+    public IEnumerator FadeOutCoroutine()
+    {
+        
+
+        Debug.Log("Started fading out...");
+
+       
+        albedoColor.a = 0;
+
+        while (alphaState > 0.01f)
+        {
+            foreach (var child in gameObject.GetAllChilds())
+            {
+                child.GetComponent<MeshRenderer>().material.color = Color.Lerp(child.GetComponent<MeshRenderer>().material.color, albedoColor, fadeOutSpeed * Time.deltaTime);
+                alphaState = child.GetComponent<MeshRenderer>().material.color.a;
+                Debug.Log(alphaState);
+            }
+
+            yield return new WaitForEndOfFrame();
+            //Debug.Log("mpika stin while");
+        }
+
+            if (DestroyOnFadeOut == true)
+            {
+                Destroy(this);
+            }
+            else if (DisableOnFadeOut == true)
+            {
+                this.gameObject.SetActive(false);
+            }
+
+        StartFadeOut = false;
+        FadeOutPending = false;
+    }
+}
